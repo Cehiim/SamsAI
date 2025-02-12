c4d80bd46eeb541939e8384257304b6d2e5aeec3
david-pessoa <pessoa.dvlbp@gmail.com>
2025-02-12 16:41:56 -0300

    cria models

--- backend/application/admin.py
+++ backend/application/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+from .models import Mensagem, Conversa
 
-# Register your models here.
+
+@admin.register(Conversa)
+class ConversaAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'usuario', 'nome')
+
+@admin.register(Mensagem)
+class MnesagemAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'conversa', 'inicio_texto', 'data', 'eh_do_usuario')
--- backend/application/models.py
+++ backend/application/models.py
@@ -1,4 +1,30 @@
+from tabnanny import verbose
 from django.db import models
+from django.contrib.auth.models import User
 
+class Conversa(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.PROTECT, related_name="conversas", null=True)
+    nome = models.CharField("Nome", max_length=30)
 
-aaa
+    def __str__(self):
+        return self.nome
+    
+    class Meta:
+        verbose_name = "Conversa"
+        verbose_name_plural = "Conversas"
+
+class Mensagem(models.Model):
+    conversa = models.ForeignKey(Conversa, on_delete=models.PROTECT, related_name="mensagens", null=True)
+    texto = models.TextField("Texto", max_length=10000)
+    data = models.DateTimeField("Data e horário da mensagem", auto_now_add=True)
+    eh_do_usuario = models.BooleanField("Mensagem do usuário?", default=True)
+
+    def inicio_texto(self):
+        return self.texto[:30] + "..." if len(self.texto) > 30 else self.texto  
+
+    def __str__(self):
+        return self.texto[:10] + "..."
+    
+    class Meta:
+        verbose_name = "Mensagem"
+        verbose_name_plural = "Mensagens"
