eff117b29b8d4ef017bccab4fdc44d9819d106da
david-pessoa <pessoa.dvlbp@gmail.com>
2025-02-12 17:55:28 -0300

    frontend vai ser em javascript puro, sem React

--- backend/application/admin.py
+++ backend/application/admin.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.models import User
-from django.contrib.auth.admin import UserAdmin
 from .models import Mensagem, Conversa
-
 
 @admin.register(Conversa)
 class ConversaAdmin(admin.ModelAdmin):
